4fa363dc4760069573ff64528999440a31c7e4b8
Roman <karosen1448@gmail.com>
2025-10-31 15:55:17 +0300

    fix: build

--- src/shared/lib/utils/items.tsx
+++ src/shared/lib/utils/items.tsx
@@ -1,5 +1,5 @@
 import { Home, Calendar, Book, User } from "lucide-react";
-import { Lesson, MenuItemType } from "../types";
+import { MenuItemType } from "../types";
 import { paths } from "./paths";
 
 export const menuItems: MenuItemType[] = [
